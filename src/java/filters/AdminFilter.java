76cc0a052bda7f4ced51e8de1a41af3bf1f91d06
791662 <791662@ELF8IT221576.ACDM.DS.SAIT.CA>
2020-11-09 18:36:15 -0700

    Validating admin access Validate if users are able to access the admin areas of the website

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -10,6 +10,11 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+import services.AccountService;
 
 /**
  *
@@ -18,12 +23,32 @@ import javax.servlet.ServletResponse;
 public class AdminFilter implements Filter {
     
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain)
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+            
+        HttpServletRequest hsrequest = (HttpServletRequest)request;
+        HttpSession session = hsrequest.getSession();
         
+        HttpServletResponse hsresponse = (HttpServletResponse)response;
         
-            chain.doFilter(request, response);
+        AccountService as = new AccountService();
+        
+        String email = (String)session.getAttribute("email");
+        String password = (String)session.getAttribute("password");
+        
+        User user = as.login(email, password);
+        
+        if(user == null) {
+            hsresponse.sendRedirect("login");
+            return;
+        }
+        
+        if(user.getRole().getRoleId() != 1) {
+            hsresponse.sendRedirect("notes");
+            return;
+        }
+        
+        chain.doFilter(request, response);
         
     }
 
